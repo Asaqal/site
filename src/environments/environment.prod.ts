946fe922ba144e86c84566213cdb62d66038bcc3
Ahmad Saqal <saqalahmad@gmail.com>
2021-12-25 19:17:24 -0500

    attempted to fix firebase sdk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAGRXG8Ia1rwNpFMLyh4EcIgLLHZ8InZSI',
+    authDomain: 'asaqal-site.firebaseapp.com',
+    databaseURL: 'https://asaqal-site-default-rtdb.firebaseio.com',
+    projectId: 'asaqal-site',
+    storageBucket: 'asaqal-site.appspot.com',
+    messagingSenderId: '567295461535',
+    appId: '1:567295461535:web:bb70d0c8a120ba37a73aaa',
+  }
 };
