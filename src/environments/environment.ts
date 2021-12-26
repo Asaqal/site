bfe31fe7c8ea101e5bd389230ffbbf95314ad9f1
Ahmad Saqal <saqalahmad@gmail.com>
2021-12-25 19:05:12 -0500

    added firebase sdk

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAGRXG8Ia1rwNpFMLyh4EcIgLLHZ8InZSI',
+    authDomain: 'asaqal-site.firebaseapp.com',
+    databaseURL: 'https://asaqal-site-default-rtdb.firebaseio.com',
+    projectId: 'asaqal-site',
+    storageBucket: 'asaqal-site.appspot.com',
+    messagingSenderId: '567295461535',
+    appId: '1:567295461535:web:bb70d0c8a120ba37a73aaa',
+  }
 };
 
 /*
