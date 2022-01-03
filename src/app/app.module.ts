1742a98fa15baa87d1afe4098abffa7e7da8c9bb
Ahmad Saqal <saqalahmad@gmail.com>
2022-01-03 17:12:48 -0500

    restructured project folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProjectOverviewComponent } from './project/project-overview/project-overview.component';
-import { ProjectDetailsComponent } from './project/project-details/project-details.component';
+import { ProjectViewerComponent } from './project-viewer/project-viewer.component';
+import { ProjectOverviewComponent } from './project-viewer/project-overview/project-overview.component';
+import { ProjectDetailsComponent } from './project-viewer/project-details/project-details.component';
 @NgModule({
   declarations: [
     AppComponent,
+    ProjectViewerComponent,
     ProjectOverviewComponent,
-    ProjectDetailsComponent
+    ProjectDetailsComponent,
   ],
   imports: [
     BrowserModule,
